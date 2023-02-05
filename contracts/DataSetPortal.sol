45db6b0a43748840782cd6740b1fc97778ccc925
skywalkerAlex <alexdim.liak@gmail.com>
2023-02-05 19:50:33 +0200

    Eth Contract completed + test

---
+++ contracts/DataSetPortal.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract DataSetPortal {
+    uint256 totalDatasets;
+    uint256 totalSize;
+    /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
+
+    /*
+     * It stores the arguments passed in transaction logs.
+     * These logs are stored on blockchain and are accessible
+     * using address of the contract till the contract is present on the blockchain.
+     * An event generated is not accessible from within contracts,
+     * not even the one which have created and emitted them.
+     */
+    event NewDataset(
+        address indexed from,
+        uint256 timestamp,
+        DatasetObj dataset
+    );
+
+    /*
+     * A custom datatype where we can customize what we want to hold inside it.
+     */
+    struct DatasetObj {
+        address user_address; // The address of the user.
+        string name; // The name the user.
+        uint256 size; // The size of the dataset in MB.
+        string accuracy_score; // The accuracy score of the model.
+        string data_type; // The type of the dataset.
+        string file_type; // The type of the files on the dataset.
+        string[] models_used; // Whitch models are used.
+        string[] libraries_used; // Whitch libraries are used in the model.
+        uint256 timestamp; // The timestamp when the user upload the dataset.
+    }
+
+    /*
+     * A custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Total {
+        uint256 numberOfDataset; // The number of DataSets.
+        uint256 size; // The size of the dataset in MB.
+    }
+
+    /*
+     * I declare a variable datasets that lets me store an array of structs.
+     * This is what lets me hold all the datasets anyone ever sends to me!
+     */
+    DatasetObj[] datasets;
+
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user uploaded a dataset in our contract.
+     */
+    mapping(address => uint256) public lastUploadAt;
+
+    constructor() payable {
+        console.log("I AM SMART CONTRACT. POG. And payable.");
+        console.log("Yo yo, I am a contract and I am smart\n");
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function upload_dataset_details(
+        string memory name,
+        uint256 size,
+        string memory accuracy_score,
+        string memory data_type,
+        string memory file_type,
+        string[] memory models_used,
+        string[] memory libraries_used
+    ) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        console.log("upload_dataset_details");
+        require(
+            lastUploadAt[msg.sender] + 15 seconds < block.timestamp,
+            "Must wait 30 seconds before uplaoding again."
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastUploadAt[msg.sender] = block.timestamp;
+        DatasetObj memory _datasetInput;
+        _datasetInput.name = name;
+        _datasetInput.size = size;
+        _datasetInput.accuracy_score = accuracy_score;
+        _datasetInput.data_type = data_type;
+        _datasetInput.file_type = file_type;
+        _datasetInput.models_used = models_used;
+        _datasetInput.libraries_used = libraries_used;
+
+        console.log("_datasetInput.name : %s", _datasetInput.name);
+        console.log(
+            "%s sender w/ accuracy score %s",
+            msg.sender,
+            _datasetInput.accuracy_score
+        );
+
+        _datasetInput.user_address = msg.sender;
+        _datasetInput.timestamp = block.timestamp;
+        /*
+         * This is where I actually store the dataset data in the array.
+         */
+        datasets.push(_datasetInput);
+
+        totalDatasets += 1;
+        totalSize += _datasetInput.size;
+        /*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewDataset(msg.sender, block.timestamp, _datasetInput);
+    }
+
+    /*
+     * I added a function getAllDatasets which will return the struct array, datasets, to us.
+     * This will make it easy to retrieve the datasets from our website!
+     */
+    function getAllDatasets() public view returns (DatasetObj[] memory) {
+        return datasets;
+    }
+
+    function getTotalNumbers() public view returns (Total memory) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
+        console.log("We have %d total datasets!", totalDatasets);
+
+        return Total(totalDatasets, totalSize);
+    }
+
+    // function uintToString(uint256 v) private returns (string str) {
+    //     uint256 maxlength = 100;
+    //     bytes memory reversed = new bytes(maxlength);
+    //     uint256 i = 0;
+    //     while (v != 0) {
+    //         uint256 remainder = v % 10;
+    //         v = v / 10;
+    //         reversed[i++] = bytes1(48 + remainder);
+    //     }
+    //     bytes memory s = new bytes(i + 1);
+    //     for (uint256 j = 0; j <= i; j++) {
+    //         s[j] = reversed[i - j];
+    //     }
+    //     str = string(s);
+    // }
+}
--- scripts/run.js
+++ scripts/run.js
@@ -1,67 +1,97 @@
 const main=async () => {
     const [owner, randomPerson]=await hre.ethers.getSigners();
-    const waveContractFactory=await hre.ethers.getContractFactory("WavePortal");
-    const waveContract=await waveContractFactory.deploy({
+    const datasetContractFactory=await hre.ethers.getContractFactory("DataSetPortal");
+    const datasetContract=await datasetContractFactory.deploy({
         value: hre.ethers.utils.parseEther("0.1"),
     });
-    await waveContract.deployed();
+    await datasetContract.deployed();
 
-    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed to:", datasetContract.address);
     console.log("Contract deployed by:", owner.address);
 
     /*
     * Get Contract balance
     */
     let contractBalance=await hre.ethers.provider.getBalance(
-        waveContract.address
+        datasetContract.address
     );
     console.log(
         "Contract balance:",
         hre.ethers.utils.formatEther(contractBalance)
     );
 
-    let waveCount=await waveContract.getTotalWaves();
-    console.log("WaveCount: "+waveCount.toNumber());
+    let totals=await datasetContract.getTotalNumbers();
+    console.log("Total Numbers of Dataset: "+totals.numberOfDataset);
 
+    let inputValue1={
+        name: "name1",
+        size: 15,
+        accuracy_score: "0.9",
+        data_type: "dataType1",
+        file_type: "fileType1",
+        models_used: ["model1", "model2", "model3"],
+        libraries_used: ["library1", "library2", "library3"],
+    }
 
-    const firstWaveTxn=await waveContract.wave("First wave");
-    await firstWaveTxn.wait(); // Wait for the transaction to be mined
+    console.log("inputValue1: "+inputValue1);
+    const firstDataset=await datasetContract.upload_dataset_details(
+        inputValue1.name,
+        inputValue1.size,
+        inputValue1.accuracy_score,
+        inputValue1.data_type,
+        inputValue1.file_type,
+        inputValue1.models_used,
+        inputValue1.libraries_used,
+    );
+    await firstDataset.wait(); // Wait for the transaction to be mined
 
-    waveCount=await waveContract.getTotalWaves();
-    console.log("WaveCount: "+waveCount.toNumber());
+    totals=await datasetContract.getTotalNumbers();
+    console.log("Total Numbers of Dataset: "+totals.numberOfDataset);
 
     /*
     * Get Contract balance to see what happened!
     */
-    contractBalance=await hre.ethers.provider.getBalance(waveContract.address);
+    contractBalance=await hre.ethers.provider.getBalance(datasetContract.address);
     console.log(
         "Contract balance:",
         hre.ethers.utils.formatEther(contractBalance)
     );
 
-    const secondWaveTxn=await waveContract.connect(randomPerson).wave("Second wave!");
-    await secondWaveTxn.wait();
+    let inputValue2={
+        name: "name2",
+        data_type: "dataType2",
+        accuracy_score: "accuracyScore2",
+        file_type: "fileType2",
+        size: 32,
+        models_used: ["model4", "model5", "model6"],
+        libraries_used: ["library4", "library5", "library6"],
+    }
+    const secondDataset=await datasetContract.connect(randomPerson).upload_dataset_details(
+        inputValue2.name,
+        inputValue2.size,
+        inputValue2.accuracy_score,
+        inputValue2.data_type,
+        inputValue2.file_type,
+        inputValue2.models_used,
+        inputValue2.libraries_used,
+    );
+    await secondDataset.wait();
 
-    waveCount=await waveContract.getTotalWaves();
-    console.log("WaveCount: "+waveCount.toNumber());
-
-    console.log("remove wave");
-    waveContract.unWave();
-
-    waveCount=await waveContract.getTotalWaves();
-    console.log("WaveCount: "+waveCount.toNumber());
+    totals=await datasetContract.getTotalNumbers();
+    console.log("Total Numbers of Dataset: "+totals.numberOfDataset);
+    console.log("Total size of Dataset in MB: "+totals.size);
 
     /*
     * Get Contract balance to see what happened!
     */
-    contractBalance=await hre.ethers.provider.getBalance(waveContract.address);
+    contractBalance=await hre.ethers.provider.getBalance(datasetContract.address);
     console.log(
         "Contract balance:",
         hre.ethers.utils.formatEther(contractBalance)
     );
 
-    let allWaves=await waveContract.getAllWaves();
-    console.log("AllWaves: "+allWaves);
+    let allDatasets=await datasetContract.getAllDatasets();
+    console.log("All Datasets: "+allDatasets);
 };
 
 const runMain=async () => {
